48ca314b72f571aee9051f984ce73741a158f6b1
sharmanirudh <sharmanirudh27@gmail.com>
2018-07-12 07:03:37 +0530

    first commit

---
+++ app/src/main/java/com/example/sharm/beautifulgraphlibrary/MainActivity.java
@@ -0,0 +1,253 @@
+package com.example.sharm.beautifulgraphlibrary;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RelativeLayout;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.Utils;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private static final String TAG = "MainActivity";
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private  Sensor sensors;
+
+    private LineChart mChart;
+    private Thread thread;
+    private boolean plotData = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+        List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        for(int i=0; i<sensors.size(); i++){
+            Log.d(TAG, "onCreate: Sensor "+ i + ": " + sensors.get(i).toString());
+        }
+
+        if (mAccelerometer != null) {
+            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+        }
+
+        mChart = findViewById(R.id.chart1);
+
+        // enable description text
+        mChart.getDescription().setEnabled(false);
+//        Description desc = new Description();
+//        desc.setText("dB");
+//        mChart.setDescription(desc);
+
+        // enable touch gestures
+        mChart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        mChart.setPinchZoom(true);
+
+        // set an alternative background color
+        mChart.setBackgroundColor(getResources().getColor(R.color.colorBackgroundGraph));
+
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+
+        // add empty data
+        mChart.setData(data);
+
+        // get the legend (only possible after setting data)
+        Legend l = mChart.getLegend();
+
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.NONE);
+        l.setTextColor(Color.WHITE);
+
+        XAxis xl = mChart.getXAxis();
+        xl.setTextColor(Color.WHITE);
+        xl.setAxisLineColor(Color.TRANSPARENT);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setPosition(XAxis.XAxisPosition.BOTTOM);
+//        xl.setXOffset(8f);
+        xl.setYOffset(-36f);
+        xl.setEnabled(true);
+//        xl.setTypeface();
+//        xl.setTextSize();
+//        xl.setTextColor();
+//        xl.setAxisLineColor();
+//        xl.setGridColor();
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setAxisLineColor(Color.TRANSPARENT);
+        leftAxis.setGridColor(Color.BLACK);
+        leftAxis.setAxisMaximum(120f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setDrawZeroLine(false);
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART);
+//        leftAxis.setXOffset(20f);
+        leftAxis.setYOffset(-30f);
+//        leftAxis.setCenterAxisLabels(false);
+
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+        mChart.getAxisLeft().setDrawGridLines(true);
+        mChart.getXAxis().setDrawGridLines(false);
+        mChart.setDrawBorders(false);
+        mChart.setExtraOffsets(0,0,0,0);
+//        mChart.setBorderColor(Color.WHITE);
+//        mChart.setBorderWidth(5f);
+
+        feedMultiple();
+
+    }
+
+    private void addEntry(SensorEvent event) {
+
+        LineData data = mChart.getData();
+
+        if (data != null) {
+
+            ILineDataSet set = data.getDataSetByIndex(0);
+            // set.addEntry(...); // can be called as well
+
+            if (set == null) {
+                set = createSet();
+                data.addDataSet(set);
+            }
+
+//            data.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 80) + 10f), 0);
+            data.addEntry(new Entry(set.getEntryCount(), event.values[0] + 60), 0);
+            data.notifyDataChanged();
+
+            // let the chart know it's data has changed
+            mChart.notifyDataSetChanged();
+
+            // limit the number of visible entries
+            mChart.setVisibleXRangeMaximum(140);
+            // mChart.setVisibleYRange(30, AxisDependency.LEFT);
+
+            // move to the latest entry
+            mChart.moveViewToX(data.getEntryCount());
+
+        }
+    }
+
+    private LineDataSet createSet() {
+
+        LineDataSet set = new LineDataSet(null, "");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setLineWidth(1f);
+        set.setColor(Color.WHITE);
+        set.setHighlightEnabled(false);
+        set.setDrawValues(false);
+        set.setDrawCircles(false);
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+
+        //myEdit starts
+        set.setDrawFilled(true);
+        if (Utils.getSDKInt() >= 18) {
+            // fill drawable only supported on api level 18 and above
+            Drawable drawable = ContextCompat.getDrawable(this, R.drawable.amin);
+            set.setFillDrawable(drawable);
+        }
+        else {
+            set.setFillColor(Color.BLUE);
+        }
+        //myEdit ends
+
+        return set;
+    }
+
+    private void feedMultiple() {
+
+        if (thread != null){
+            thread.interrupt();
+        }
+
+        thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true){
+                    plotData = true;
+                    try {
+                        Thread.sleep(40);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (thread != null) {
+            thread.interrupt();
+        }
+        mSensorManager.unregisterListener(this);
+
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        if(plotData){
+            addEntry(event);
+            plotData = false;
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mSensorManager.unregisterListener(MainActivity.this);
+        thread.interrupt();
+        super.onDestroy();
+    }
+}
+
